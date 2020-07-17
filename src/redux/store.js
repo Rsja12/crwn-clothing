a9b0cfe231b0109017f2526b7192ba9e547173df
Rsja12 <asocarras10@gmail.com>
2020-07-17 17:01:04 -0400

    remove logger from prod

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,11 @@ import logger from 'redux-logger'
 
 import rootReducer from './root-reducer'
 
-const middlewares = [logger]
+const middlewares = []
+
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger)
+}
 // spread in all values from middlewares as individual arguments to applyMiddleware()
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 export const persistor = persistStore(store)
