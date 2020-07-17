50bccd747e71276aef0324c5afd2b3427dd56ed3
Rsja12 <asocarras10@gmail.com>
2020-07-17 17:01:46 -0400

    remove logger from prod

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,6 +9,7 @@ const middlewares = []
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger)
 }
+
 // spread in all values from middlewares as individual arguments to applyMiddleware()
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 export const persistor = persistStore(store)
