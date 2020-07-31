ffc7cc9a854ceddd6396cf4efa6c743a0ee03a73
Rsja12 <asocarras10@gmail.com>
2020-07-31 16:50:00 -0400

    add server for stripe

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
